afc294a7c18bf7e244dcfcbc35379d546151cd57
Yuliya <macbook@MacBook-Pro-Yuliya.local>
2020-12-11 20:04:29 +0300

    Initial commit

---
+++ fridayChallange_002.py
@@ -0,0 +1,17 @@
+'''
+Если выписать все натуральные числа меньше 10, кратные 3 или 5, то получим 3, 5, 6 и 9.
+Сумма этих чисел равна 23.
+Найдите сумму всех чисел меньше 1000, кратных 3 или 5.
+'''
+
+
+sum = 0
+rng = 1000  #desired range
+n3 = 3      #number 1
+n5 = 5      #number 2
+
+for i in range(rng):
+    if not i%n3 or not i%n5:
+        sum += i
+
+print ("Sum of integer numbers in range of", rng, "which fully divide by",n3, "and", n5, "is equal to", sum)
