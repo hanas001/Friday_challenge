b5e927297df8e608e64472d52409a8e4817b40b7
Eugene <eugene.golubenko@gmail.com>
2020-12-23 00:17:17 +0300

    commented out a line

--- fridayChellenge_003.py
+++ fridayChellenge_003.py
@@ -5,6 +5,6 @@ for i in range(len(string)+1):
     if i > 0:
         newString.append(string[-i])
 
-print (type(newString))
+#print (type(newString))
 #print (newString, sep="")  #for python 3
 print "".join(newString)    #for python 2.7
