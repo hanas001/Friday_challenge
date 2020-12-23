8f202bcf177f252aa919547894a68e4b4dc978d8
hanas001 <hanas001@ya.ru>
2020-12-23 14:46:21 +0300

    added user input corrected python 3 output

--- fridayChellenge_003.py
+++ fridayChellenge_003.py
@@ -1,10 +1,10 @@
-string = 'hello, how are you?'
+#string = 'hello, how are you?'  #to test
+string = input()                #user input
 newString = []
 
 for i in range(len(string)+1):
     if i > 0:
         newString.append(string[-i])
 
-#print (type(newString))
-print (newString, sep='')  #for python 3
-#print "".join(newString)    #for python 2.7
+print (''.join(newString))  #for python 3
+#print ''.join(newString)    #for python 2.7
