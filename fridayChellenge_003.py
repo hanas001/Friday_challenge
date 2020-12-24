a033f96889fe54028f7a5dd326bac35080acc271
hanas001 <hanas001@ya.ru>
2020-12-24 12:45:40 +0300

    one input for python 2 and 3

--- fridayChellenge_003.py
+++ fridayChellenge_003.py
@@ -1,10 +1,10 @@
 #string = 'hello, how are you?'         #to test
-#string = input()                       #user input in python 3
-string = raw_input()                    #user input in python 2
+string = input()                       #user input in python 3
+#string = raw_input()                    #user input in python 2
 newString = []
 
 for i in range(len(string)+1):
     if i > 0:
         newString.append(string[-i])
 
-print (''.join(newString))      #for python 3
+print (''.join(newString))
